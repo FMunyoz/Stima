d0026efb8429713b9d18d3f5f7c23e7bd3ca0d70
FMunyoz <federico.munyoz@gmail.com>
2015-03-24 16:14:24 +0100

    Versión 1.0 con ejecución de todos

--- EstimarTasacion.R
+++ EstimarTasacion.R
@@ -48,11 +48,10 @@ SeleccionarOtrasVariables <- function(Muestra){
                                             "NumeroDePlanta", 
                                             "DatosDeLaPlanta"))))
   #Actualiza Datos Auxiliares
-  Datos[,"NumeroDeAscensores"][is.na(Datos[,"NumeroDeAscensores"])] <- 1
-  Datos[,"ExteriorInterior"][is.na(Datos[,"ExteriorInterior"])] <- 1
-  Datos[,"DatosDeLaPlanta"][is.na(Datos[,"DatosDeLaPlanta"])] <- 7
-  Datos[,"DatosDeLaPlanta"] <- ifelse(Datos[,"DatosDeLaPlanta"] == 0, 5, 7)
-  
+  Datos[is.na(Datos[,"NumeroDeAscensores"]),"NumeroDeAscensores"] <- 1
+  Datos[is.na(Datos[,"ExteriorInterior"]),"ExteriorInterior"] <- 1
+  Datos[is.na(Datos[,"DatosDeLaPlanta"]),"DatosDeLaPlanta"] <- 7
+
   Datos
   
 }
@@ -82,15 +81,45 @@ ObtieneOtrasVariables <- function(MatrizOtrasVariables){
   cbind(MatrizOtrasVariables, SinAscensor, EsPlantaBaja, EsSemiSotano, EsInterior, EsAtico, NDePlanta)
 }
 
-CorreccionPorOtrasVariables <- function(VariablesDeLaMuestra, CasoEstimacion, Coeficientes){
+CorreccionPorOtrasVariables <- function(VariablesDeLaMuestra, CasoEstimacion, Parametros){
   MatrizCasoEstimacion <- CasoEstimacion[rep(row.names(CasoEstimacion), nrow(VariablesDeLaMuestra)), 1:10]
+  Beta <- subset(Parametros, Tipologia=="Plurifamiliar" & Tipo=="beta", select = c(11:16))
+  B <- subset(Parametros, Tipologia=="Plurifamiliar" & Tipo=="b", select = c(11:16))
+  SinAscensor <- exp((-VariablesDeLaMuestra[,"SinAscensor"]+MatrizCasoEstimacion[,"SinAscensor"])*Beta$SinAscensor)
+  EsPlantaBaja <- exp((-VariablesDeLaMuestra[,"EsPlantaBaja"]+MatrizCasoEstimacion[,"EsPlantaBaja"])*Beta$EsPlantaBaja)
+  EsSemiSotano <- exp((-VariablesDeLaMuestra[,"EsSemiSotano"]+MatrizCasoEstimacion[,"EsSemiSotano"])*Beta$EsSemiSotano)
+  EsInterior <- exp((-VariablesDeLaMuestra[,"EsInterior"]+MatrizCasoEstimacion[,"EsInterior"])*Beta$EsInterior)
+  EsAtico <- 0
+  #Calculo de si es atico
+  for (Muestra in 1:nrow(VariablesDeLaMuestra)){
+    if (CasoEstimacion[,"SinAscensor"] == FALSE){
+      if(VariablesDeLaMuestra[Muestra,"SinAscensor"] == FALSE){
+        EsAtico[Muestra] <- exp((-VariablesDeLaMuestra[Muestra,"EsAtico"]+CasoEstimacion[,"EsAtico"])*Beta$EsAtico)      
+      }
+      else {
+        EsAtico[Muestra] <- exp(+CasoEstimacion[,"EsAtico"]*Beta$EsAtico)          
+      }
+    }
+    else {
+      if(VariablesDeLaMuestra[Muestra,"SinAscensor"] == FALSE){
+        EsAtico[Muestra] <- exp((-VariablesDeLaMuestra[Muestra,"EsAtico"])*Beta$EsAtico)
+      }
+      else {
+        EsAtico[Muestra] <- 1      
+      }
+    }
+  }
   
-  SinAscensor <- exp((-VariablesDeLaMuestra[,"SinAscensor"]+MatrizCasoEstimacion[,"SinAscensor"])*Coeficientes$SinAscensor)
-  EsPlantaBaja <- exp((-VariablesDeLaMuestra[,"EsPlantaBaja"]+MatrizCasoEstimacion[,"EsPlantaBaja"])*Coeficientes$pbaja)
-  EsSemiSotano <- exp((-VariablesDeLaMuestra[,"EsSemiSotano"]+MatrizCasoEstimacion[,"EsSemiSotano"])*Coeficientes$pbaja)
-  EsInterior <- exp((-VariablesDeLaMuestra[,"EsInterior"]+MatrizCasoEstimacion[,"EsInterior"])*Coeficientes$interior)
-  EsAtico <- exp((-VariablesDeLaMuestra[,"EsAtico"]+MatrizCasoEstimacion[,"EsAtico"])*Coeficientes$Atico)
-  lPlanta <- exp((-VariablesDeLaMuestra[,"NDePlanta"]+MatrizCasoEstimacion[,"NDePlanta"])*Coeficientes$lplanta)
+  lPlanta <- 0
+  #Calculo del Logaritmo de la planta
+  for (Muestra in 1:nrow(VariablesDeLaMuestra)){
+    if (CasoEstimacion[,"SinAscensor"] == FALSE){
+      lPlanta[Muestra] <- ((CasoEstimacion[,"NDePlanta"]+1)/(VariablesDeLaMuestra[Muestra,"NDePlanta"]+1))^B$NDePlanta     
+    }
+    else {
+      lPlanta[Muestra] <- exp(Beta$NDePlanta)^(CasoEstimacion[,"NDePlanta"]^2 - VariablesDeLaMuestra[Muestra,"NDePlanta"]^2)
+    }
+  }
   
   SinAscensor * EsPlantaBaja * EsSemiSotano * EsInterior * EsAtico * lPlanta
 }
--- GeneracionTasacion.R
+++ GeneracionTasacion.R
@@ -1,38 +1,40 @@
-#Directorio = "C:/Users/federico.munyoz/Documents/TINSA/Documentos/WEB_1314917"
-Directorio = "C:/Users/federico.munyoz/Documents/TINSA/Documentos/WEB_1314921"
-#Directorio = "C:/Users/federico.munyoz/Documents/TINSA/Documentos/WEB_1315155"
+Tasacion <- function(Directorio){
     ListaDeDatos <- LeerArchivos(Directorio)
 
-    TipologiaA_Estimar <- ListaDeDatos$Estimacion[,"Tipologia"]
+    TipologiaEstimacion <- ListaDeDatos$Estimacion[,"Tipologia"]
         
-    Variables <- SeleccionarVariables(ListaDeDatos$Muestra, TipologiaA_Estimar)
-    VariablesCaso <- SeleccionarVariables(ListaDeDatos$Estimacion, TipologiaA_Estimar)
+    Variables <- SeleccionarVariables(ListaDeDatos$Muestra, TipologiaEstimacion)
+    VariablesCaso <- SeleccionarVariables(ListaDeDatos$Estimacion, TipologiaEstimacion)
     
     #Datos Auxiliares
-    OtrasVariables <- SeleccionarOtrasVariables(ListaDeDatos$Muestra)
     OtrasVariablesCaso <- SeleccionarOtrasVariables(ListaDeDatos$Estimacion)
+    OtrasVariables <- SeleccionarOtrasVariables(ListaDeDatos$Muestra)
     
     #Hay que poner este valor si es 1, 2 ó 7
-    OtrasVariablesCaso[,"NumeroDePlanta"][is.na(OtrasVariablesCaso[,"NumeroDePlanta"])] <- 0.411764706
+    ElementosConValor7 <- sum(OtrasVariables[,"DatosDeLaPlanta"] == 7)
+    DatosASumar <- OtrasVariables[,"DatosDeLaPlanta"] == 7
+    TotalDePlantaParaElementosConValor7 <- sum(OtrasVariables[DatosASumar,"NumeroDePlanta"])
+    
+    OtrasVariablesCaso[is.na(OtrasVariablesCaso[,"NumeroDePlanta"]), "NumeroDePlanta"] <- TotalDePlantaParaElementosConValor7/ElementosConValor7
+    if(OtrasVariablesCaso[,"DatosDeLaPlanta"] == 0) OtrasVariablesCaso[,"DatosDeLaPlanta"] <-  5
     
     LogaritmoVariablesCaso <- TransformarVariables(VariablesCaso)
     LogaritmoVariables <- TransformarVariables(Variables)
     
-    BetaOtrasVariables <- subset(ListaDeDatos$Parametros, Tipologia=="Plurifamiliar" & Tipo=="beta", select = c(11:16))
-    
-    Coeficientes <- ObtieneDatosDelModelo(ListaDeDatos$Parametros, TipologiaA_Estimar)
-    
-    ValoresMinimos <- t(as.matrix(apply(LogaritmoVariables, 2, min)))
-    ValoresMaximos <- t(as.matrix(apply(LogaritmoVariables, 2, max)))
-    Rangos <- ValoresMaximos - ValoresMinimos
-    rownames(Rangos) <- "1"
+    Coeficientes <- ObtieneDatosDelModelo(ListaDeDatos$Parametros, TipologiaEstimacion)
     Medias <- t(as.matrix(apply(LogaritmoVariables, 2, mean)))
-    
-    B_Unifamiliar <- subset(ListaDeDatos$Parametros, Tipologia=="Unifamiliar" & Tipo=="b", select = c(4:9))
 
-    # Se asigna valor a CosteDeConstruccionBrutoUnitarioVivienda porque no tiene datos
+# Se asigna valor a CosteDeConstruccionBrutoUnitarioVivienda porque no tiene datos
     
     LogaritmoVariablesCaso[1,"CosteDeConstruccionBrutoUnitarioVivienda"] = Medias[1,"CosteDeConstruccionBrutoUnitarioVivienda"]
+    
+    
+    ValoresMinimos <- t(as.matrix(apply(rbind(LogaritmoVariables, LogaritmoVariablesCaso), 2, min)))
+    ValoresMaximos <- t(as.matrix(apply(rbind(LogaritmoVariables, LogaritmoVariablesCaso), 2, max)))
+    Rangos <- ValoresMaximos - ValoresMinimos
+    rownames(Rangos) <- "1"
+    B_Unifamiliar <- subset(ListaDeDatos$Parametros, Tipologia=="Unifamiliar" & Tipo=="b", select = c(4:9))
+
     
     CotaDistancia <- (LogaritmoVariablesCaso / (1000 * Rangos))^2
     # 
@@ -42,11 +44,17 @@ Directorio = "C:/Users/federico.munyoz/Documents/TINSA/Documentos/WEB_1314921"
     MatrizDatosCaso <- LogaritmoVariablesCaso[rep(row.names(LogaritmoVariablesCaso), nrow(Variables)), 1:6]
     MatrizRangos <- Rangos[rep(row.names(Rangos), nrow(Variables)), 1:6]
     MatrizCoeficientes <- Coeficientes[rep(row.names(Coeficientes), nrow(Variables)), 1:6]
-    Matriz_b <- B_Unifamiliar[rep(row.names(B_Unifamiliar), nrow(Variables)), 1:6]
+    if (TipologiaEstimacion==1){
+      Matriz_b <- subset(ListaDeDatos$Parametros, Tipologia=="Unifamiliar" & Tipo=="b", select = c(4:9))
+    } else {
+      Matriz_b <- subset(ListaDeDatos$Parametros, Tipologia=="Plurifamiliar" & Tipo=="b", select = c(4:9))      
+    }
+    Matriz_b <- Matriz_b[rep(row.names(Matriz_b), nrow(Variables)), 1:6]
+                       
     Distancia <- as.matrix(rowSums(((LogaritmoVariables[,2:6] - MatrizDatosCaso[,2:6])/MatrizRangos[,2:6])^2 * MatrizCoeficientes[,1:5]),na.rm = TRUE)
     colnames(Distancia) <- "Distancia"
     
-    Ly_Transformada <- as.matrix(LogaritmoVariables[,1] + rowSums((MatrizDatosCaso[,2:6] - LogaritmoVariables[,2:6])*Matriz_b[,1:5]),na.rm = TRUE)
+    Ly_Transformada <- as.matrix(LogaritmoVariables[,"ValorUnitarioDeMercado"] + rowSums((MatrizDatosCaso[,2:6] - LogaritmoVariables[,2:6])*Matriz_b[,1:5]),na.rm = TRUE)
     colnames(Ly_Transformada) <- "Ly_Transformada"
     
     jerarquia <- as.matrix(order(Distancia))
@@ -72,10 +80,10 @@ Directorio = "C:/Users/federico.munyoz/Documents/TINSA/Documentos/WEB_1314921"
     
     OtrasVariables <- ObtieneOtrasVariables(OtrasVariables)
     
-    CorreccionOtrasVariables <- CorreccionPorOtrasVariables(OtrasVariables, OtrasVariablesCaso, BetaOtrasVariables)
+    CorreccionOtrasVariables <- CorreccionPorOtrasVariables(OtrasVariables, OtrasVariablesCaso, ListaDeDatos$Parametros)
 
     Correccion <- CorreccionOtrasVariables[rownames(MuestraOrdenada)]
-    wCorregido <- ifelse(MuestraOrdenada[,"Tipologia"]==1, MuestraOrdenada[,"Ly_Transformada"],log(MuestraOrdenada[,"Ly_Transformada"]*exp(Correccion)))
+    wCorregido <- ifelse(MuestraOrdenada[,"Tipologia"]==1, MuestraOrdenada[,"Ly_Transformada"],log(Correccion*exp(MuestraOrdenada[,"Ly_Transformada"])))
     PesosXwCorregido <- MuestraOrdenada[,"Pesos"]*wCorregido
     PesosXwCorregidoCuadrado <-  MuestraOrdenada[,"Pesos"]*wCorregido^2
     SumaPesos <- cumsum(MuestraOrdenada[,"Pesos"])
@@ -85,13 +93,14 @@ Directorio = "C:/Users/federico.munyoz/Documents/TINSA/Documentos/WEB_1314921"
     SumaPesosXwCorregidoCuadradoEntreSumaPesos <- SumaPesosXwCorregidoCuadrado/SumaPesos
     s <- sqrt(SumaPesos*round(SumaPesosXwCorregidoCuadradoEntreSumaPesos-Estimacion^2,8)/(SumaPesos-1))
     rndSumaPesosMenos1 <- round(SumaPesos-1)
+    rndSumaPesosMenos1 <- ifelse(rndSumaPesosMenos1 == 0, 1, rndSumaPesosMenos1)
     t <- ifelse(rndSumaPesosMenos1>30, rndSumaPesosMenos1,qt(.975, 1:30)[rndSumaPesosMenos1])
     sxt <- s*t
     MuestraOrdenada <- cbind(MuestraOrdenada, Correccion, wCorregido, PesosXwCorregido, PesosXwCorregidoCuadrado, SumaPesos, SumaPesosXwCorregido, 
                              Estimacion, SumaPesosXwCorregidoCuadrado, SumaPesosXwCorregidoCuadradoEntreSumaPesos, s, rndSumaPesosMenos1, t, sxt)
     
 
-    MuestraOrdenadaFiltrada <- tail(MuestraOrdenada, 25)
+    MuestraOrdenadaFiltrada <- MuestraOrdenada[6:30,]
     ValorDistribucionT <- ifelse(min(MuestraOrdenadaFiltrada[,"sxt"])>=log(1.05), min(MuestraOrdenadaFiltrada[,"sxt"]), MuestraOrdenadaFiltrada[,"sxt"][25])
     
     Seleccionados <- head(MuestraOrdenada, 5)
@@ -100,3 +109,17 @@ Directorio = "C:/Users/federico.munyoz/Documents/TINSA/Documentos/WEB_1314921"
     Seleccionados <- rbind(Seleccionados, MuestrasAdicionales)
     
     GrabarTasacion(Directorio, Seleccionados)
+}
+
+#Tasacion( "C:/Users/federico.munyoz/Documents/TINSA/Documentos/WEB_1314917")
+#Tasacion( "C:/Users/federico.munyoz/Documents/TINSA/Documentos/WEB_1314921")
+#Tasacion( "C:/Users/federico.munyoz/Documents/TINSA/Documentos/WEB_1314922")
+#Tasacion( "C:/Users/federico.munyoz/Documents/TINSA/Documentos/WEB_1315155")
+#Tasacion( "C:/Users/federico.munyoz/Documents/TINSA/Documentos/WEB_1315178")
+#Tasacion("C:/Users/federico.munyoz/Documents/TINSA/Documentos/WEB_1315296")
+#Tasacion("C:/Users/federico.munyoz/Documents/TINSA/Documentos/WEB_1315298")
+#Tasacion("C:/Users/federico.munyoz/Documents/TINSA/Documentos/WEB_1315299")
+#Tasacion("C:/Users/federico.munyoz/Documents/TINSA/Documentos/WEB_1315301")
+#Tasacion("C:/Users/federico.munyoz/Documents/TINSA/Documentos/WEB_1315307")
+#Tasacion("C:/Users/federico.munyoz/Documents/TINSA/Documentos/WEB_1315311")
+Tasacion("C:/Users/federico.munyoz/Documents/TINSA/Documentos/WEB_1315323")
